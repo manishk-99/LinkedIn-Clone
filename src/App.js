1ed205a29957679982c8d05e333914729d779a59
Manish Kumar <66010479+manishk-99@users.noreply.github.com>
2021-05-14 23:23:54 +0530

    Base-Completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import {HashRouter, Switch, Route} from "react-router-dom"
+import { HashRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Login from "./components/Login"
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login";
 
 export default function App() {
   return (
@@ -9,6 +11,10 @@ export default function App() {
         <Switch>
           <Route exact path="/">
             <Login />
+          </Route>
+          <Route path="/home">
+            <Header />
+            <Home />
           </Route>
         </Switch>
       </HashRouter>
