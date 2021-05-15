d4160fbd3736eac7acaa93e609276285d3b464be
Manish Kumar <66010479+manishk-99@users.noreply.github.com>
2021-05-15 16:06:14 +0530

    home-added

--- src/components/Home.js
+++ src/components/Home.js
@@ -67,7 +67,6 @@ const Layout = styled.div`
   grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
   column-gap: 25px;
   row-gap: 25px;
-  /* grid-template-row: auto; */
   margin: 25px 0;
   @media (max-width: 768px) {
     display: flex;
